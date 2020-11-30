0c925ed8bbdd0a2b1843c4bea275dbcb97ec0531
Sam <1791694111@qq.com>
2020-11-30 10:56:00 +0800

    对象的自动导入以及主辅配置文件的使用

---
+++ springlearn/SpringLearn/Step_Three/src/main/java/cool/smhw/bg03/Student.java
@@ -0,0 +1,29 @@
+package cool.smhw.bg03;
+
+public class Student {
+
+    private String name;
+    private Integer age;
+    private School school;
+
+    public void setSchool(School school) {
+        this.school = school;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", school=" + school +
+                '}';
+    }
+}
--- springlearn/SpringLearn/Step_Three/src/test/java/cool/smhw/MyTest.java
+++ springlearn/SpringLearn/Step_Three/src/test/java/cool/smhw/MyTest.java
@@ -1,14 +1,23 @@
 package cool.smhw;
 
-import cool.smhw.bg02.Student;
+import cool.smhw.bg03.Student;
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class MyTest {
+//    @Test
+//    public void test01(){
+//        String config = "beans.xml";
+//        ApplicationContext ac = new ClassPathXmlApplicationContext(config);
+//
+//        Student student = (Student)ac.getBean("student");
+//        System.out.println(student);
+//    }
+
     @Test
-    public void test01(){
-        String config = "beans.xml";
+    public void test02(){
+        String config = "bg03/spring-total.xml";
         ApplicationContext ac = new ClassPathXmlApplicationContext(config);
 
         Student student = (Student)ac.getBean("student");
